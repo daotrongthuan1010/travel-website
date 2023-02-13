84c4346b9772f24e3fedb913cfeefc89a860b834
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-02-13 22:23:38 +0700

    update file config

--- travel-social/src/main/java/daothuan/web/com/travelsocial/TravelSocialApplication.java
+++ travel-social/src/main/java/daothuan/web/com/travelsocial/TravelSocialApplication.java
@@ -2,6 +2,7 @@ package daothuan.web.com.travelsocial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TravelSocialApplication {
