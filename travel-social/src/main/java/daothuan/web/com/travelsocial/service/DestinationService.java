d4e6d24b851081f72ab198da5bffc281207d4655
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-02-19 23:47:39 +0700

    update field for tables and hanlder logic service and repository

---
+++ travel-social/src/main/java/daothuan/web/com/travelsocial/service/DestinationService.java
@@ -0,0 +1,8 @@
+
+package daothuan.web.com.travelsocial.service;/**
+ * @author thuandao1010
+ * @version 1.0
+ * @since 2023-02-11
+ */
+public class DestinationService {
+}
--- travel-social/src/main/java/daothuan/web/com/travelsocial/service/UserSecurityService.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author ThuanDao
- */
-package daothuan.web.com.travelsocial.service;
-
-public class UserSecurityService {
-
-}
