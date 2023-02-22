11e16064d9d4abff357db89c28d4780bc25dd5ca
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-02-22 22:57:00 +0700

    update error constant

--- travel-social/src/main/java/daothuan/web/com/travelsocial/exception/TravelCustomException.java
+++ travel-social/src/main/java/daothuan/web/com/travelsocial/exception/TravelCustomException.java
@@ -1,5 +1,9 @@
 package daothuan.web.com.travelsocial.exception;
+import daothuan.web.com.travelsocial.exception.entityerror.ValidationErrorResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,14 +16,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequiredArgsConstructor
 public class TravelCustomException extends RuntimeException{
 
+  private static final String RESULT_NG = "ng";
+
+  private MessageSource messageSource;
+
   @ExceptionHandler({ServiceException.class})
   public void ServiceException(){
 
   }
 
   @ExceptionHandler({ValidationException.class})
-  public void ValidationException(){
+  public ResponseEntity<ValidationErrorResponse> ValidationException(
+      jakarta.validation.ValidationException validationException){
 
+    ValidationErrorResponse.ValidationError validationError = ValidationErrorResponse.ValidationError
+        .builder()
+        .title(messageSource.getMessage(MessagePro))
+
+    ValidationErrorResponse response = ValidationErrorResponse.builder()
+        .result(RESULT_NG)
+        .validationErrors()
+
+  return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
   }
 
 }
---
+++ travel-social/src/main/java/daothuan/web/com/travelsocial/utils/MessagePropertiesConstants.java
@@ -0,0 +1,31 @@
+package daothuan.web.com.travelsocial.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author thuandao1010
+ * @version 1.0
+ * @since 2023-02-11
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MessagePropertiesConstants {
+
+  public static final String RESPONSE_VALIDATION_400 = "response.jakarta.message.validation.400";
+
+  public static final String RESPONSE_400 = "response.jakarta.message.400";
+
+  public static final String RESPONSE_401 = "response.jakarta.message.401";
+
+  public static final String RESPONSE_403 = "response.jakarta.message.403";
+
+  public static final String RESPONSE_404 = "response.jakarta.message.404";
+
+  public static final String RESPONSE_409 = "response.jakarta.message.409";
+
+  public static final String RESPONSE_500 = "response.jakarta.message.500";
+
+
+
+
+}
